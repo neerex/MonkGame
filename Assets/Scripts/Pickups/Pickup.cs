8377490da1d9b2082176941a29e42757fe47705e
IlyaLeontev <neerex101@yandex.ru>
2023-04-25 16:44:21 +0300

    Pickup vfx
    - added new pick up looks
    - added vfx on pick up

--- Assets/Scripts/Pickups/Pickup.cs
+++ Assets/Scripts/Pickups/Pickup.cs
@@ -4,14 +4,27 @@ namespace MainGame.Pickups
 {
     public class Pickup : MonoBehaviour
     {
+        [SerializeField] private ParticleSystem _applyVfx;
+        
         public void PerformPickup(GameObject pickupRequester)
         {
-            var pickupEffects = GetComponents<PickupEffect>();
+            PickupEffect[] pickupEffects = GetComponents<PickupEffect>();
             foreach (var effect in pickupEffects)
             {
                 effect.ApplyEffect(pickupRequester);
             }
+
+            InstantiateApplyVfs();
             Destroy(gameObject);
+        }
+
+        private void InstantiateApplyVfs()
+        {
+            if (_applyVfx != null)
+            {
+                ParticleSystem particles = Instantiate(_applyVfx, transform.position, Quaternion.identity);
+                particles.transform.localScale *= 3f;
+            }
         }
     }
 }
